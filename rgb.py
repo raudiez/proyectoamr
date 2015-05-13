65237f1e56ba643e62b01473897e81ed8961f26b
Raúl Díez Sánchez <rauldiez20@gmail.com>
2015-05-13 11:02:09 +0200

    Cambiado nombre, añadido PWM a rgb.

--- rgb.py
+++ rgb.py
@@ -29,15 +29,23 @@ GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 
-# Clear all existing color values.
-GPIO.output(red, 0)
-GPIO.output(green, 0)
-GPIO.output(blue, 0)
+# Set up colors using PWM so we can control individual brightness.
+RED = GPIO.PWM(red, 100)
+GREEN = GPIO.PWM(green, 100)
+BLUE = GPIO.PWM(blue, 100)
+RED.start(0)
+GREEN.start(0)
+BLUE.start(0)
 
-# Set individual colors.
-if args.color[0] == 'red':
-  GPIO.output(red, 1)
-elif args.color[0] == 'green':
-  GPIO.output(green, 1)
-elif args.color[0] == 'blue':
-  GPIO.output(blue, 1)
+# Set a color by giving R, G, and B values of 0-255.
+def setColor(rgb = []):
+    # Convert 0-255 range to 0-100.
+    rgb = [(x / 255.0) * 100 for x in rgb]
+    RED.ChangeDutyCycle(rgb[0])
+    GREEN.ChangeDutyCycle(rgb[1])
+    BLUE.ChangeDutyCycle(rgb[2])
+
+setColor(args.rgb)
+time.sleep(10)
+
+GPIO.cleanup()
